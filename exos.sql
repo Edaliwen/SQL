d75d4592d5199a7f4a21097cb997d1197f11321d
Edaliwen <amandine.valtat@gmail.com>
2023-01-02 10:22:43 +0100

    Ajout fichier énoncé + exercices
    Ajout du fichier des énoncés des exercices
    Exercices 1&2

---
+++ exos.sql
@@ -0,0 +1,12 @@
+/* Partie 1 */
+-- commentaire --
+
+/* Exercice 1 */
+CREATE DATABASE webDevelopment CHARACTER SET utf8;
+
+/* Exercice 2 */
+CREATE TABLE languages (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `language` varchar(20),
+    PRIMARY KEY (`id`)
+);
